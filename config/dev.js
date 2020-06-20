2c7de1356218fd1b88b7d78fcccbdfc525656d93
Thiago Moreira <thiagosm.004@gmail.com>
2020-06-20 02:01:18 -0300

    add nodemon and hidden some informations

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://boilerplate-user:boilerplate-user@cluster0-0pyhz.gcp.mongodb.net/boilerplate?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+// Bring the key.js file to check if we are on production or dev mode
+const config = require('./config/key');
+
+// Bring the user model that we created
+const {User} = require('./models/user');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://boilerplate-user:boilerplate-user@cluster0-0pyhz.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+mongoose.connect(config.mongoURI, 
     {useNewUrlParser: true,  useUnifiedTopology: true}).then(()=> console.log('DB connected...'))
                             .catch(err => console.error(err));
 
 
-app.get('/', (req, res)=>{
-    res.send('hello world1');
-});
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Routes
+app.get('/', (req, res)=> {
+    res.json({"Hello ~": "thiagos2"})
+})
+
+app.post('/api/users/register', (req, res) =>{
+    const user = new User(req.body)
+
+    user.save((err, userData) =>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+    
+})
 
 
 
